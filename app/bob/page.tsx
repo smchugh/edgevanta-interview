29624e3b39cb0653d37f0f9aac447043234404e6
Steve McHugh <steven.e.mchugh@gmail.com>
2024-04-30 15:25:05 -0500

    Send appropriate sender with messages

--- app/bob/page.tsx
+++ app/bob/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { MessageTerminal } from "@/components/ui/message-terminal";
+
 export default function Page() {
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
       <h1 className="text-4xl font-extrabold my-8 text-center">
         Bob
       </h1>
-      TODO
+      <MessageTerminal sender="bob" />
     </main>
   );
 }
