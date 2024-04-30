1aed235fd4c9460f9d875852a017a76db790baf1
Steve McHugh <steven.e.mchugh@gmail.com>
2024-04-30 16:00:47 -0500

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Code } from "@/components/typography/code";
 import { Link } from "@/components/typography/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -12,7 +11,6 @@ import {
   useSignOut,
   useSignUpSignIn,
 } from "@convex-dev/convex-lucia-auth/react";
-import { useMutation } from "convex/react";
 
 export default function Home() {
   const sessionId = useSessionId();
@@ -20,7 +18,7 @@ export default function Home() {
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
       <h1 className="text-4xl font-extrabold my-8 text-center">
-        Convex + Next.js + Lucia Auth
+        Edgevanta Test
       </h1>
       {sessionId ? <SignedIn /> : <AuthForm />}
     </main>
@@ -28,11 +26,7 @@ export default function Home() {
 }
 
 function SignedIn() {
-  const { viewer, numbers } =
-    useQueryWithAuth(api.myFunctions.listNumbers, {
-      count: 10,
-    }) ?? {};
-  const addNumber = useMutation(api.myFunctions.addNumber);
+  const { viewer } = useQueryWithAuth(api.myFunctions.getUser, {}) ?? {};
 
   return (
     <>
@@ -41,50 +35,16 @@ function SignedIn() {
         <SignOutButton />
       </p>
       <p>
+        Choose the appropriate message terminal to send messages as that user.
+      </p>
+      <p>
         <Link href="/alice">
-          Alice
+          Alice&apos;s Message Terminal
         </Link>
       </p>
       <p>
         <Link href="/bob">
-          Bob
-        </Link>
-      </p>
-      <p>
-        Click the button below and open this page in another window - this data
-        is persisted in the Convex cloud database!
-      </p>
-      <p>
-        <Button
-          onClick={() => {
-            void addNumber({ value: Math.floor(Math.random() * 10) });
-          }}
-        >
-          Add a random number
-        </Button>
-      </p>
-      <p>
-        Numbers:{" "}
-        {numbers?.length === 0
-          ? "Click the button!"
-          : numbers?.join(", ") ?? "..."}
-      </p>
-      <p>
-        Edit <Code>convex/myFunctions.ts</Code> to change your backend
-      </p>
-      <p>
-        Edit <Code>app/page.tsx</Code> to change your frontend
-      </p>
-      <p>
-        Check out{" "}
-        <Link target="_blank" href="https://docs.convex.dev/home">
-          Convex docs
-        </Link>
-      </p>
-      <p>
-        To build a full page layout copy one of the included{" "}
-        <Link target="_blank" href="/layouts">
-          layouts
+          Bob&apos;s Message Terminal
         </Link>
       </p>
     </>
